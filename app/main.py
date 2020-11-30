be16e91c63d2cf1256af196c7be342b750a5b889
carlos <carlos@pruebas.europe-west1-b.c.ampso-290916.internal>
2020-11-30 17:59:54 +0000

    Cambio

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello_world():
     #return ("Helo, world!")
-    message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+    message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON. Cambio"
     response = {
             "message": message,
             "length": len(message)
