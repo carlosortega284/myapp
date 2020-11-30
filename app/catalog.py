6854e55f3a2509a7e3a78470249e7f9e322595ff
carlos <carlos@pruebas.europe-west1-b.c.ampso-290916.internal>
2020-11-30 19:47:24 +0000

    Anyadido metodo post tambien

---
+++ app/catalog.py
@@ -0,0 +1,20 @@
+import sys
+import uuid
+
+def get_products():
+    fake_response = [{
+            "sku": uuid.uuid4(),
+            "title": "Titulo",
+            "long_description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+            "price_euro": 1.5
+    },{
+            "sku": uuid.uuid4(),
+            "title": "Titulo2",
+            "long_description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+            "price_euro": 0.5
+    }]
+    return fake_response
+
+def create_product(sku, title, long_description, price_euro):
+    '''Insertar todo en una bbdd'''
+    print(f"Crear sku={sku} y title={title}", file=sys.stderr)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from flask import jsonify
-from flask import Flask
+from flask import jsonify, request, Flask
+from catalog import get_products, create_product
+
 app = Flask(__name__)
+
+@app.route('/product', methods=['GET', 'POST'])
+def list_all_products():
+    '''This view maneges the CRUD of products'''
+    if request.method == 'GET':
+        response = get_products()
+        return jsonify(response)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        create_product(
+            data['sku'],
+            data['title'],
+            data['long_description'],
+            data['price_euro'])
+        return jsonify({"status": "ok"})
 
 @app.route('/hello')
 def hello_world():
